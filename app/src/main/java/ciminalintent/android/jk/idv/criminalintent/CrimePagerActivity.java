cbf7eb59a14aa557aa84b11987b0f98834dabb06
javakidx <javakidx@gmail.com>
2015-07-24 17:41:09 +0800

    created

---
+++ app/src/main/java/ciminalintent/android/jk/idv/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,47 @@
+package ciminalintent.android.jk.idv.criminalintent;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.List;
+
+/**
+ * Created by ¦w¶a on 2015/7/24.
+ */
+public class CrimePagerActivity extends FragmentActivity
+{
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        mViewPager = new ViewPager(this);
+        mViewPager.setId(R.id.viewPager);
+        setContentView(mViewPager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentPagerAdapter(fm)
+        {
+            @Override
+            public Fragment getItem(int position)
+            {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount()
+            {
+                return mCrimes.size();
+            }
+        });
+    }
+}
